bebda125f17c744423f59e8f62967bc80128e863
Xazeq <setezey@gmail.com>
2021-09-09 10:51:52 +0300

    Init travis & jacoco

---
+++ src/test/java/ru/job4j/TriggerTest.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TriggerTest {
+
+    @Test
+    public void someLogic() {
+        Assert.assertEquals(1, new Trigger().someLogic());
+    }
+}
