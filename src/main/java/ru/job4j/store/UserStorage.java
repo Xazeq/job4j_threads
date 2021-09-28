4813f9b152ea4416c7e367848a4cb4a75ddaf63f
Xazeq <setezey@gmail.com>
2021-09-28 09:19:48 +0300

    3. Класс хранилища пользователей UserStorage [#1104 #207747]

--- src/main/java/ru/job4j/store/UserStorage.java
+++ src/main/java/ru/job4j/store/UserStorage.java
@@ -4,39 +4,39 @@ import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @ThreadSafe
 public class UserStorage implements Storage {
     @GuardedBy("this")
-    private final List<User> users = new ArrayList<>();
+    private final Map<Integer, User> users;
+
+    public UserStorage(Map<Integer, User> users) {
+        this.users = users;
+    }
 
     @Override
     public synchronized boolean add(User user) {
-        return users.add(user);
+        return users.putIfAbsent(user.getId(), user) == null;
     }
 
     @Override
     public synchronized boolean update(User user) {
-        boolean result = false;
-        int index = users.indexOf(user);
-        if (index != -1) {
-            users.set(index, user);
-            result = true;
-        }
-        return result;
+        return users.replace(user.getId(), user) != null;
     }
 
     @Override
     public synchronized boolean delete(User user) {
-        return users.remove(user);
+        return users.remove(user.getId(), user);
     }
 
     @Override
     public synchronized boolean transfer(int fromId, int toId, int amount) {
         boolean result = false;
-        User userFrom = findById(fromId);
-        User userTo = findById(toId);
+        User userFrom = users.get(fromId);
+        User userTo = users.get(toId);
         if (userFrom != null && userTo != null && userFrom.getAmount() >= amount) {
             userFrom.setAmount(userFrom.getAmount() - amount);
             userTo.setAmount(userTo.getAmount() + amount);
@@ -45,21 +45,12 @@ public class UserStorage implements Storage {
         return result;
     }
 
-    private synchronized User findById(int id) {
-        for (var user : users) {
-            if (user.getId() == id) {
-                return user;
-            }
-        }
-        return null;
-    }
-
     public synchronized List<User> getUsers() {
-        return users;
+        return new ArrayList<>(users.values());
     }
 
     public static void main(String[] args) {
-        UserStorage storage = new UserStorage();
+        UserStorage storage = new UserStorage(new HashMap<>());
         storage.add(new User(1, 100));
         storage.add(new User(2, 200));
         storage.transfer(1, 2, 50);
